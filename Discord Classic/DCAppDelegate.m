dfd844ec7bfc9723961ba42184f419c7e9247d8a
Trevir <triveri@outlook.com>
2018-03-18 20:46:40 -0400

    workaround for disconnect when exiting app

--- Discord Classic/DCAppDelegate.m
+++ Discord Classic/DCAppDelegate.m
@@ -10,16 +10,13 @@
 
 @implementation DCAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
 	
 	return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)applicationWillResignActive:(UIApplication *)application{
+	exit(0);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
